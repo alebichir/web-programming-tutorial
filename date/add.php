88c8bf1822874354426ad19b35b8c71a6023225b
Alexandra <alexandrabichir@yahoo.com>
2017-07-27 01:10:10 +0300

    intro to php lab 9

--- date/add.php
+++ date/add.php
@@ -3,6 +3,25 @@
     <meta http-equiv="Refresh" content="5;url=../contacte.html">
 </head>
 <body>
+
+
+<?php
+$contentstring = file_get_contents("contacte.json");
+$contacte = json_decode($contentstring, true);
+
+$newperson = array(
+    "firstName" =>  $_GET["firstName"],
+    "lastName" => $_GET["lastName"],
+    "phone" => $_GET["phone"],
+    "id" => 5
+);
+
+$contacte[] = $newperson;
+$contentstring = json_encode($contacte, true);
+file_put_contents("contacte.json", $contentstring);
+?>
+
+
 contact added (<div id="contact-id"></div>);
 <script>
     var parameters = location.search.substr(1);
